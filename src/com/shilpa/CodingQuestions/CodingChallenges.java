9abe2c0af43ec0e2933e066155be51c2daecebf6
Shilpa Raveendran <silpa304@gmail.com>
2024-12-18 00:03:56 -0500

    More Coding questions

--- src/com/shilpa/CodingQuestions/CodingChallenges.java
+++ src/com/shilpa/CodingQuestions/CodingChallenges.java
@@ -13,6 +13,18 @@ public class CodingChallenges {
         findSecondHighest();
         containsWord("cat");
         sumOfSquaresOfOddNo();
+        findCommon();
+    }
+
+    //Common Elements Between Two Lists
+    private static void findCommon() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
+        List<Integer> list2 = Arrays.asList(3, 4, 5, 6);
+
+        List<Integer> common=list1.stream()
+                .filter(list2::contains)
+                .collect(Collectors.toList());
+        System.out.println(common);
     }
 
     private static void sumOfSquaresOfOddNo() {
